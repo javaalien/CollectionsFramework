721db358e307dc877c9bfb0199b1bcbf1b706e15
Chetan Malabade <malabade.chetan@gmail.com>
2024-07-10 00:43:07 +0530

    hashmap programs

---
+++ src/main/java/com/alien/map/HashMapEntryKeySetValues.java
@@ -0,0 +1,29 @@
+package com.alien.map;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapEntryKeySetValues {
+
+	public static void main(String[] args) {
+		Map<String, String> countryISOCodeMapping = new HashMap<>();
+
+		countryISOCodeMapping.put("India", "IN");
+		countryISOCodeMapping.put("United States of America", "US");
+		countryISOCodeMapping.put("Russia", "RU");
+		countryISOCodeMapping.put("Japan", "JP");
+		countryISOCodeMapping.put("China", "CN");
+
+		Set<Map.Entry<String, String>> countryISOCodeEntries = countryISOCodeMapping.entrySet();
+		System.out.println("countryISOCode entries : " + countryISOCodeEntries);
+
+		Set<String> countries = countryISOCodeMapping.keySet();
+		System.out.println("countries : " + countries);
+
+		Collection<String> isoCodes = countryISOCodeMapping.values();
+		System.out.println("isoCodes : " + isoCodes);
+	}
+
+}
