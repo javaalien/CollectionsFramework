e2cb87fd5a73dbd9d7590ab1c810d74aca2280f5
Chetan Malabade <malabade.chetan@gmail.com>
2024-07-11 22:55:59 +0530

    linkedlist programs

---
+++ src/main/java/com/alien/list/linkedlist/RetrieveLinkedListElements.java
@@ -0,0 +1,28 @@
+package com.alien.list.linkedlist;
+
+import java.util.LinkedList;
+
+public class RetrieveLinkedListElements {
+
+	public static void main(String[] args) {
+
+		LinkedList<Double> stockPrices = new LinkedList<>();
+
+		stockPrices.add(45.00);
+		stockPrices.add(51.00);
+		stockPrices.add(62.50);
+		stockPrices.add(42.75);
+		stockPrices.add(36.80);
+		stockPrices.add(68.40);
+
+		Double firstElement = stockPrices.getFirst();
+		System.out.println("Initial Stock Price : " + firstElement);
+
+		Double lastElement = stockPrices.getLast();
+		System.out.println("Current Stock Price : " + lastElement);
+
+		Double stockPriceOn3rdDay = stockPrices.get(2);
+		System.out.println("Stock Price on 3rd Day : " + stockPriceOn3rdDay);
+
+	}
+}
---
+++ src/main/java/com/alien/list/linkedlist/SearchLinkedList.java
@@ -0,0 +1,25 @@
+package com.alien.list.linkedlist;
+
+import java.util.LinkedList;
+
+public class SearchLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<String> employees = new LinkedList<>();
+
+		employees.add("John");
+		employees.add("David");
+		employees.add("Lara");
+		employees.add("Chris");
+		employees.add("Steve");
+		employees.add("David");
+
+		System.out.println("Does Employees LinkedList contain \"Lara\"? : " + employees.contains("Lara"));
+
+		System.out.println("indexOf \"Steve\" : " + employees.indexOf("Steve"));
+		System.out.println("indexOf \"Mark\" : " + employees.indexOf("Mark"));
+
+		System.out.println("lastIndexOf \"David\" : " + employees.lastIndexOf("David"));
+		System.out.println("lastIndexOf \"Bob\" : " + employees.lastIndexOf("Bob"));
+	}
+}
